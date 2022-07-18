cf0638cefbf0e07e027880a8f1e7557577fa12e5
JaviDomin <javimc4233@gmail.com>
2022-07-17 23:48:48 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'hola';
 const nuevoNum = 'numero';
 
 // Crea una variable booleana:
-const nuevoBool = 'True';
+const nuevoBool = 'falso';
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
