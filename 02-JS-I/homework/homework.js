ac82abe26998f8373ce360a463427e427124af30
JaviDomin <javimc4233@gmail.com>
2022-07-18 16:59:06 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -59,7 +59,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  return true
+  sonIguales(false);
 }
 
 function tienenMismaLongitud(str1, str2) {
