de5d38719994459a8fffbfee81a1f22caca96492
JaviDomin <javimc4233@gmail.com>
2022-07-18 21:02:39 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -76,7 +76,10 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
- return (num) = false;
+  if (num == 80)
+       return true;
+        else if (num == 100);
+      return false;
 }
 
 function mayorQueCincuenta(num) {
