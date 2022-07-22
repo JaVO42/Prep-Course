38043d6c01e9eba6ac29c78e15b7b7e5468436ad
JaviDomin <javimc4233@gmail.com>
2022-07-22 18:48:49 -0300

    tiene tres digitos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -166,8 +166,13 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
-}
+  if (numero === 111) {
+    return true;
+  } else {
+    return false;
+  }
+   
+ }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
