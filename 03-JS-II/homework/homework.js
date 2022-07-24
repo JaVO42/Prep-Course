e9fa1926c0edf0c8b6de61ed21d4d34b32fc9202
JaviDomin <javimc4233@gmail.com>
2022-07-24 01:40:21 -0300

    tabla6

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -163,8 +163,12 @@ function esVerdadero(valor){
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
-  
+  //Escribe tu código aquí 
+ let arraytabla6 = []
+  for (let i = 0; i < 11; i++) {
+    arraytabla6.push(6 * i)
+  }
+     return arraytabla6
 }
 
 function tieneTresDigitos(numero){
