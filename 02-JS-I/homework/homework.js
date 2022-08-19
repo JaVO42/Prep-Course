cdb61fac232b3a32f42d48ae975c5e02e02a9999
JaviDomin <javimc4233@gmail.com>
2022-08-19 02:16:13 -0300

    area rectangulo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -200,7 +200,7 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  return area * ancho;
+  return alto * ancho;
 }
 
 
